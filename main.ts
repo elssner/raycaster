91a4f0900b07a38e744871a5fd1b1a51cc5d032f
GTA Calliope <73078489+elssner@users.noreply.github.com>
2024-05-01 21:07:01 +0200

    Aktualisiere pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,8 @@
 matrix.init(matrix.ePages.y128)
+matrix.displayMatrix()
 
-const map = ["##########",
+const map = [
+    "##########",
     "#        #",
     "#        #",
     "#        #",
@@ -9,23 +11,24 @@ const map = ["##########",
     "#        #",
     "#        #",
     "#        #",
-    "##########"];
+    "##########"
+];
 
 let player_pos = [2, 2];
 let player_dir = 0;
-let player_fov = matrix.pi() / 3;
+let player_fov = Math.PI / 3;
 
 function render() {
     matrix.clearMatrix();
 
     let ray_step = 0.1;
-    
-    for(let i = 0; i < 128; i++) {
+
+    for (let i = 0; i < 128; i++) {
         let ray_angle = player_dir - player_fov / 2 + i * player_fov / 128;
         let ray_pos = player_pos;
         let ray_dist = 0;
 
-        while(map[Math.floor(ray_pos[1])][Math.floor(ray_pos[0])] == " ") {
+        while (map[Math.floor(ray_pos[1])][Math.floor(ray_pos[0])] == " ") {
             ray_pos[0] += Math.cos(player_dir) * ray_step;
             ray_pos[1] += Math.sin(player_dir) * ray_step;
             ray_dist += ray_step;
@@ -33,30 +36,39 @@ function render() {
 
         let wall_height = 128 / (ray_dist + 0.01);
 
-        let color = 255;
-        if(color > 255) {
+        /* let color = 255;
+        if (color > 255) {
             color = 255;
         }
-        if(color < 0) {
+        if (color < 0) {
             color = 0;
-        }
+        } */
 
-        if(wall_height > 63) {
+        if (wall_height > 63) {
             wall_height = 63
         }
-
-        matrix.line(i, 128 / 2 - wall_height / 2, i, 128 / 2 + wall_height / 2, true);
+        //  basic.showNumber(128 / 2 + wall_height / 2)
+        matrix.line(
+            i,
+            128 / 2 - wall_height / 2,
+            i,
+            128 / 2 + wall_height / 2,
+            true, true);
+        matrix.displayMatrix();
     }
 
-    matrix.displayMatrix();
+    //    matrix.displayMatrix();
 }
 
 function on_forever() {
     matrix.setPixel(0, 0, true);
 
-    render();
+    matrix.line(0, 0, 127, 127)
+    matrix.displayMatrix()
+    control.waitMicros(1000000)
 
-    if(input.buttonIsPressed(Button.B)) {
+    render();
+    if (input.buttonIsPressed(Button.B)) {
         player_dir = player_dir + 1;
     }
     if (input.buttonIsPressed(Button.A)) {
